37b4510882039f6314ed7a63afcc19e369593168
Peter Nijssen <peter@peternijssen.nl>
2016-02-01 01:26:37 +0100

    fixed unit test

--- tests/Model/SesIdentityTest.php
+++ tests/Model/SesIdentityTest.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace PeterNijssen\Ses\Model;
-
+namespace PeterNijssen\Ses\Tests\Model;
 
 use PeterNijssen\Ses\Exception\InvalidIdentityException;
+use PeterNijssen\Ses\Model\SesIdentity;
 
 class SesIdentityTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testCreateInstance()
     {
-        $this->assertInstanceOf(SesIdentity::class, new SesIdentity("test@test.com"));
-        $this->assertInstanceOf(SesIdentity::class, new SesIdentity("test.com"));
+        $this->assertInstanceOf("PeterNijssen\Ses\Model\SesIdentity", new SesIdentity("test@test.com"));
+        $this->assertInstanceOf("PeterNijssen\Ses\Model\SesIdentity", new SesIdentity("test.com"));
 
-        $this->assertInstanceOf(SesIdentity::class, new SesIdentity("test@test.photography"));
-        $this->assertInstanceOf(SesIdentity::class, new SesIdentity("test.photography"));
+        $this->assertInstanceOf("PeterNijssen\Ses\Model\SesIdentity", new SesIdentity("test@test.photography"));
+        $this->assertInstanceOf("PeterNijssen\Ses\Model\SesIdentity", new SesIdentity("test.photography"));
 
-        $this->assertInstanceOf(SesIdentity::class, new SesIdentity("test@test.co.uk"));
-        $this->assertInstanceOf(SesIdentity::class, new SesIdentity("test.co.uk"));
+        $this->assertInstanceOf("PeterNijssen\Ses\Model\SesIdentity", new SesIdentity("test@test.co.uk"));
+        $this->assertInstanceOf("PeterNijssen\Ses\Model\SesIdentity", new SesIdentity("test.co.uk"));
 
         foreach (["test", "123", "@test.com"] as $id) {
             try {
